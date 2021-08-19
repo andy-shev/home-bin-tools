aceb01b2e82f547237a69091c33c98f050148c1e
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-08-19 19:17:21 +0300

    ge2maintainer: Allow to pass additional options to get_maintainer.pl
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- ge2maintainer.sh
+++ ge2maintainer.sh
@@ -3,7 +3,7 @@
 #
 # Send patch series to the registered maintainers in Cc list
 # Usage:
-#	$PROG [options] <commit> [additional parameters to `git send-email`]
+#	$PROG [options] [`get_maintainer.pl` options] <commit> [`git send-email` options]
 # Options:
 #  --version VERSION	the version of the series (default 1)
 #  --count NUM		how many changes to send (default 1)
@@ -14,12 +14,13 @@
 
 count="1"
 version="1"
-
+options=""
 while [ $# -gt 1 ]; do
 	case $1 in
 		-c|--count) shift; count="$1";;
 		-v|--version) shift; version="$1";;
 		--) shift; break;;
+		--*) options="$options $1";;
 		*) break;;
 	esac
 	shift
@@ -28,7 +29,7 @@ done
 [ $# -ge 1 ] || exit 1
 COMMIT="$1"; shift
 
-OPTS="--no-roles --no-rolestats --remove-duplicates --git --git-min-percent=67"
+OPTS="--no-roles --no-rolestats --remove-duplicates --git --git-min-percent=67 $options"
 
 to=$(git show -$count "$COMMIT" | scripts/get_maintainer.pl $OPTS --no-m --no-r | tr '\n' ',')
 cc=$(git show -$count "$COMMIT" | scripts/get_maintainer.pl $OPTS --no-l | tr '\n' ',')
